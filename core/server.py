2be611b6ccf067eb567d3e45297b6005bfb571e8
Lily Garcia <lunagardendev@gmail.com>
2025-09-13 11:05:27 -0400

    Server work

--- core/server.py
+++ core/server.py
@@ -1,5 +1,6 @@
 import core.wawalog
 import core.packets
+import os
 import typing
 import socket
 import json
@@ -13,6 +14,7 @@ KC_SERVER_START: str = "Server started..."
 KC_SERVER_FAIL: str = "Server failed to start!"
 KC_SERVER_BEGINNING_CONNECTION_ACCEPTING: str = "Begining to accept connections..."
 KC_SERVER_PACKET_TOO_BIG: str = "Attempted to send packet that was too big!"
+KC_SERVER_BAD_DATA: str = "Received bad data!"
 KC_SERVER_CONNECTION_STARTED: str = "Client has connected <%s>"
 KC_SERVER_CONNECTION_END: str = "Client disconnected <%s>"
 KC_SERVER_STOPPING: str = "Stopping server..."
@@ -21,15 +23,21 @@ KC_SERVER_CONSOLE_UNKNOWN_COMMAND:str = "Unknown command <%s>"
 
 SOCKET_LISTEN_TIMEOUT: float = 0.1
 
-CLIENT_SOCKET:int = 0
-CLIENT_THREAD:int = 1
-CLIENT_STOP_EVENT:int = 2
+CLIENT_SOCKET: int = 0
+CLIENT_THREAD: int = 1
+CLIENT_STOP_EVENT: int = 2
 
-USER_ID_INVALID:int = -1
+USER_ID_INVALID: int = -1
+CURRENT_ROOM_INVALID: str = ""
+
+PATH_TO_ROOM = "/data/rooms/%s"
+PATH_TO_CHUNK_IN_ROOM = "/data/rooms/%s/%s"
+CHUNK_FILE_EXTENSION = ".meow"
 
 class active_client_data:
     def __init__(self) -> None:
         self.user_id = USER_ID_INVALID
+        self.current_room = CURRENT_ROOM_INVALID
 
 class server:
     def __init__(self) -> None:
@@ -119,9 +127,9 @@ class server:
                 _id_: int = _message_[core.packets.PACKET_ID]
                 _b_data_: bytes = _message_[core.packets.PACKET_BYTE_DATA]
                 _data_: tuple = core.packets.unpack(_id_, _b_data_)
-                self.handle_data(socket, cli_data, _id_, _data_)
+                self.handle_incoming_data(socket, cli_data, _id_, _data_)
             except:
-                pass
+                core.wawalog.log(KC_SERVER_BAD_DATA)
      
     def disconnect_client(self, client: tuple) -> None:
        client[CLIENT_SOCKET].close()
@@ -134,7 +142,7 @@ class server:
             return
         socket.send(data)
     
-    def handle_data(self, socket: socket.socket, cli_data: active_client_data, id: int, data: tuple) -> None:
+    def handle_incoming_data(self, socket: socket.socket, cli_data: active_client_data, id: int, data: tuple) -> None:
         match id:
             case core.packets.P_ID_CONFIRMED:
                 pass
@@ -142,17 +150,24 @@ class server:
                 pass
             case core.packets.P_ID_REGISTER:
                 pass
-            case core.packets.P_ID_SERVER_SHUTDOWN:
-                pass
-            case core.packets.P_ID_DISCONNECTED:
+            case core.packets.P_ID_MESSAGE:
+                message_read(cli_data, data)
+            case core.packets.P_GOTO_ROOM:
                 pass
             case _:
                 pass
         pass
 
+    # ========== Incoming Data Cases ==========
+
     def message_read(self, cli_data:active_client_data, data: tuple) -> None:
         # user is not logged in!
         if cli_data.user_id == USER_ID_INVALID: return
+        # user is not in a valid room!
+        if cli_data.current_room == CURRENT_ROOM_INVALID: return
+        if not self.active_chunks.has(cli_data.current_room): return
+    
+    # =========================================
 
     def make_active_chunk(self, room_name: str) -> dict:
         return
@@ -170,11 +185,29 @@ class server:
             }
         }
     
+    #def make_message(self, room_name: str, cli )
+    
     def save_active_chunk(self, room_name: str) -> None:
-        chunk_path: str = "data/rooms/%s/%s.meow"
-        open()
-        pass
+        if not self.active_chunks.has(room_name): return
 
-    def get_current_chunk_index() -> int:
+        _chunk_file_name_ = str(self.get_current_chunk_index()) + CHUNK_FILE_EXTENSION
+        _chunk_path_ = PATH_TO_CHUNK_IN_ROOM % [room_name, _chunk_file_name_]
+        _chunk_file_ = open(_chunk_path_, "w")
 
-        pass
+        _chunk_file_.write(json.dumps(self.active_chunks[room_name]))
+
+        _chunk_file_.close()
+
+    def get_current_chunk_index(self, room_name: str) -> int:
+        _room_path_ = PATH_TO_ROOM % [room_name]
+
+        # no data for this room exists yet, which means its a brand new chunk
+        if not os.path.isdir(room_path): return 0
+
+        _chunk_index_ = 0
+
+        for i in os.scandir(room_path):
+            if not i.name.endswith(".meow"): continue
+            _chunk_index_ += 1
+
+        return _chunk_index_
